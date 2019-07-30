7ab78c85a75c55f8fcf8131c0675fcaacda9dae6
Aurélie Samyn <52289398+L0relei@users.noreply.github.com>
2019-07-30 10:33:48 +0200

    Update ConferenceDailyReset.ps1
    Création du compte administrateur sans saisie de mot de passe
    Envoi de la clé publique SSH

--- ConferenceDailyReset.ps1
+++ ConferenceDailyReset.ps1
@@ -1,7 +1,24 @@
-$adminCredential = Get-Credential -Message "Enter a username and password for the VM administrator."
+param([string]$resourceGroup)
+
 For ($i = 1; $i -le 3; $i++)
 {
-  $vmName = "VMdemo" + $i
-  Write-Host "Creating VM: " $vmName
-  New-AzVm -ResourceGroupName $resourceGroup -Name $vmName -Credential $adminCredential -Image UbuntuLTS
+    $vmName = "ConferenceDemo" + $i
+    Write-Host "Creating VM: " $vmName
+
+    # Définition compte administrateur
+
+    $SecurePassword = ConvertTo-SecureString "AJCFormation2019!" -AsPlainText -Force
+    $Credential = New-Object System.Management.Automation.PSCredential ("VMAdministrator", $SecurePassword)
+
+    # Création de la VM
+
+    New-AzVm -ResourceGroupName $resourceGroup -Name $vmName -OpenPorts 22 -Image UbuntuLTS -Credential $Credential
+
+    # Ajout de la clé publique SSH
+
+    $sshPublicKey = Get-Content "$HOME\.ssh\id_rsa.pub"
+    $vmConfig = Get-AzVM -ResourceGroupName $resourceGroup -Name $vmName
+    Add-AzVMSshPublicKey -VM $vmConfig -KeyData $sshPublicKey -Path "/home/$env:USERNAME/.ssh/authorized_keys"
+
+    $ip = Get-AzPublicIpAddress -Name $vmName -ResourceGroupName $resourceGroup | Select IpAddress
 }
